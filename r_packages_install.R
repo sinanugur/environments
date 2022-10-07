a09c6005c26bbbd2add2afa81a340ad653edbb51
Sinan Uğur Umu <sinanugur@gmail.com>
2022-10-07 12:14:30 +0200

    Create r_packages_install.R

---
+++ r_packages_install.R
@@ -0,0 +1,39 @@
+#!/usr/bin/env Rscript
+
+r = getOption("repos")
+r["CRAN"] = "http://cran.us.r-project.org"
+options(repos = r)
+
+
+packages <- c("tidyverse","optparse","librarian","Seurat","SeuratDisk","patchwork","harmony","DoubletFinder","viridis","clustree","openxlsx","topGO","org.Hs.eg.db")
+
+
+installed_packages <- packages %in% rownames(installed.packages())
+
+if (any(installed_packages == FALSE)) {
+  
+if (!requireNamespace("librarian", quietly = TRUE)) {
+  install.packages("librarian") }
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
+  install.packages("BiocManager") }
+
+if (!requireNamespace("Biobase", quietly = TRUE)) {
+  BiocManager::install("Biobase") }
+
+
+
+librarian::shelf("optparse")
+librarian::shelf("tidyverse","patchwork")
+librarian::shelf("Seurat")
+librarian::shelf('clustree')
+librarian::shelf('openxlsx')
+librarian::shelf('chris-mcginnis-ucsf/DoubletFinder')
+librarian::shelf('viridis')
+librarian::shelf('topGO')
+librarian::shelf('org.Hs.eg.db')
+
+if (!requireNamespace("SeuratDisk", quietly = TRUE)) {
+librarian::shelf("mojaveazure/seurat-disk") }
+
+}
